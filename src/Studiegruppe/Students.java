46b386011fb68d863cfc7775f8c7ce9af6666495
ellamusen <dani_1997@live.dk>
2020-09-28 10:24:36 +0200

    Deleted comments

--- src/Studiegruppe/Students.java
+++ src/Studiegruppe/Students.java
@@ -2,15 +2,6 @@ package Studiegruppe;
 
 public class Students {
 
-
-    // [X] Nu skal I selv bygge en klasse, som kan repræsentere en studerende med navn, alder, by, studieretning.
-
-    // [X] Herefter skal i instatiere "studerende-objekter" for hver studerende i Jeres gruppe .
-
-    // [] Til sidst skal I lave en switch sætning, som indlæser et navn, og som kan vise den given studerendes alder.
-
-
-
     String navn;
     int alder;
     String by;
